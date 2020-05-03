31ca198917f61a2980451bcecc8f95276bac620c
Charlie Tran <cptran14@gmail.com>
2020-05-02 18:55:33 -0700

    Install some basic dependencies

---
+++ types/global.d.ts
@@ -0,0 +1,6 @@
+// Types for compiled templates
+declare module 'portfolio-3dx20/templates/*' {
+  import { TemplateFactory } from 'htmlbars-inline-precompile';
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
