cbc876a80c91894db3f4cd94e40e3c931ad0397f
Charlie Tran <cptran14@gmail.com>
2020-05-17 16:21:21 -0700

    Basic gaming interest panel for desktop

--- app/components/desktop/interests-panel/gaming.ts
+++ app/components/desktop/interests-panel/gaming.ts
@@ -1,5 +1,38 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
+const logoImageRoot = 'assets/images/';
+
+const games = [
+  { name: 'Skyrim', asset: 'skyrim.jpg' },
+  { name: 'Final Fantasy 7', asset: 'ff7.jpg' },
+  { name: 'Tales of Berseria', asset: 'tales.jpeg' },
+  { name: 'Overwatch', asset: 'overwatch.jpg' },
+  { name: 'Vader Immortal', asset: 'vader-immortal.jpg' },
+  { name: 'Animal Crossing', asset: 'animal-crossing.jpg' },
+  { name: 'Fire Emblem', asset: 'fire-emblem.jpg' },
+  { name: 'Fate Grand Order', asset: 'fgo.jpg' }
+].map(game => ({
+  ...game,
+  asset: `${logoImageRoot}${game.asset}`
+}));
 interface DesktopInterestsPanelGamingArgs {}
 
-export default class DesktopInterestsPanelGaming extends Component<DesktopInterestsPanelGamingArgs> {}
+export default class DesktopInterestsPanelGaming extends Component<DesktopInterestsPanelGamingArgs> {
+  games = games;
+
+  @tracked
+  currentGameIndex = 0;
+
+  updateCurrentGame(): void {
+    this.currentGameIndex = (this.currentGameIndex + 1) % games.length;
+
+    setTimeout(() => this.updateCurrentGame(), 2500);
+  }
+
+  @action
+  triggerCurrentGameUpdate(): void {
+    setTimeout(() => this.updateCurrentGame(), 2500);
+  }
+}
